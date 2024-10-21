70bdce29588c8b561658d408f82c8e3085ace2b0
H-Ariel <127103172+H-Ariel@users.noreply.github.com>
2024-10-21 09:34:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const upload = multer({
+  storage: multer.memoryStorage()
+});
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file)
+    return res.status(400).json({ error: 'No file uploaded' });
+  
+  res.json({
+    name: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
